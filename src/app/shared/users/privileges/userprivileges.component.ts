0d0aff84e727223460f0c375b69ac8d7ed322959
pkazimierowski <pkazimierowski@man.poznan.pl>
2024-04-11 14:20:10 +0200

    removed options to give the  ROLE_VL_DOMAIN_ADMIN for domains

--- src/app/shared/users/privileges/userprivileges.component.ts
+++ src/app/shared/users/privileges/userprivileges.component.ts
@@ -61,7 +61,7 @@ export class UserPrivilegesComponent extends BaseComponent implements OnInit {
             roles = this.filterRoles(roles, this.newPrivilegeForm.get('domainId').value);
         } else if (this.newPrivilegeForm.get('domainId').value != null) {
             // default (domain) role set
-            roles = [Role.ROLE_GUEST, Role.ROLE_USER, Role.ROLE_DOMAIN_ADMIN, Role.ROLE_VL_DOMAIN_ADMIN];
+            roles = [Role.ROLE_GUEST, Role.ROLE_USER, Role.ROLE_DOMAIN_ADMIN];
             roles = this.filterRoles(roles, this.newPrivilegeForm.get('domainId').value);
         } else {
             // no roles
